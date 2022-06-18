bf92df6ce8f19276134e229d9972b729ae6dbc30
cnshnom <yupeiyu.cns.hn@gmail.com>
2022-06-18 11:43:53 +0200

    implement some of the button functionality.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
+import { useState } from 'react';
 
 
 const btnValues = [
@@ -13,15 +14,76 @@ const btnValues = [
   [4, 5, 6, "-"],
   [1, 2, 3, "+"],
   [0, ".", "="]
-
 ];
 
-
-
 function App() {
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
+  const numClickHandler = (e)=>{
+    e.preventDefault();
+    // e is an event object
+    // target is the object that created the event
+    // innerHTML describes the target's content X, e.g. <div>X</div>
+    const value = e.target.innerHTML;
+    setCalc(
+      {
+        ...calc, //state of all member stays the same
+        //only num and res change.
+        num:
+          calc.num === 0 && value === "0"
+          ? "0"
+          :calc.num % 1 === 0
+          ?Number(calc.num+value)
+          :calc.num+value,
+        
+        res:!calc.sign ? 0:calc.res,
+      }
+    )
+  };
+  const resetClickHandler = (e)=>{
+    e.preventDefault();
+  };
+
+  const invertClickHandler = (e)=>{
+    e.preventDefault();
+  };
+  const percentClickHandler = (e)=>{
+    e.preventDefault();
+  };
+  const equalsClickHandler = (e)=>{
+    e.preventDefault();
+  };
+
+  const signClickHandler = (e)=>{
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      sign:value,
+      res: !calc.res && calc.num?calc.num:calc.res,
+      num:0
+    });
+  };
+
+  const commaClickHandler = (e)=>{
+    e.preventDefault();
+    setCalc({
+      ...calc,
+      num:!calc.num.toString().includes(".")?calc.num+".":calc.num,
+    });
+  };
+
+  
+
+
   return (
     <Wrapper>
-      <Screen value="baum"></Screen>
+      <Screen value={calc.num>0? calc.num:calc.res}></Screen>
       <ButtonBox>
         {
           btnValues.flat().map((btn, i) => {
@@ -30,10 +92,19 @@ function App() {
                 key={i}
                 className={btn === "=" ? "equals" : ""}
                 onClick={
-                  () => {
-                    console.log(`${btn} clicked!`);
-                  }
-
+                  btn === "C"
+                  ? resetClickHandler
+                  : btn === "+-"
+                  ? invertClickHandler
+                  : btn === "%"
+                  ? percentClickHandler
+                  : btn === "="
+                  ? equalsClickHandler
+                  : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                  ? signClickHandler
+                  : btn === "."
+                  ? commaClickHandler
+                  : numClickHandler
                 }
                 value={btn}
               >
